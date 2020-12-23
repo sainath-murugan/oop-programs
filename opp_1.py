e8127c672c9ca60620d067a372d78edc7086d0ac
M.sainath <74423116+sainath-murugan@users.noreply.github.com>
2020-12-23 16:38:53 +0530

    Update opp_1.py

--- opp_1.py
+++ opp_1.py
@@ -7,12 +7,9 @@ class reverse_sentence:
 
      def reverse(self):
          return "".join(list(reversed(self.word)))
-     def reverse_a_sentence(self,):
-         return " ".join(list(map(str,self.word.split(" ")[::-1])))
-
+    
 
 constructor_1 = reverse_sentence(input("enter the word"))
-constructor_2 = reverse_sentence(input("enter the sentence"))
 
 print(constructor_1.reverse())
-print((constructor_2.reverse_a_sentence()))
+
