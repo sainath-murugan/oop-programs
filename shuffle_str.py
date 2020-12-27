4d49d97f53b96ff321735af111c9be353edd82c3
M.sainath <74423116+sainath-murugan@users.noreply.github.com>
2020-12-27 19:31:18 +0530

    Create shuffle_str.py

---
+++ shuffle_str.py
@@ -0,0 +1,13 @@
+name = "sainath"
+try:
+ random.shuffle(name)
+ print(name)
+except TypeError:  # str is not used support for shuffling
+    pass
+finally:
+
+     result = "".join(random.sample(name, len(name)))
+     print(result)
+
+
+
