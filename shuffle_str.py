224b6e10424c1ccb3895b5a9d20e5719e411d2b7
M.sainath <74423116+sainath-murugan@users.noreply.github.com>
2020-12-27 19:48:53 +0530

    Update shuffle_str.py

--- shuffle_str.py
+++ shuffle_str.py
@@ -1,11 +1,15 @@
+import random
+
 name = "sainath"
+
 try:
  random.shuffle(name)
  print(name)
+ 
 except TypeError:  # str is not used support for shuffling
     pass
+ 
 finally:
-
      result = "".join(random.sample(name, len(name)))
      print(result)
 
